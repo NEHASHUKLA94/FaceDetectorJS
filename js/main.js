ef8030d364b6dbcc211ce5dea14b86fc492e8eb6
NEHASHUKLA94 <126643062+NEHASHUKLA94@users.noreply.github.com>
2023-03-13 08:56:19 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const isScreenSmall = window.matchMedia("(max-width: 700px)");
 let predictedAges = [];
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models")
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
 ]).then(startVideo);
 
 function startVideo() {
@@ -36,25 +40,40 @@ video.addEventListener("playing", () => {
 
   setInterval(async () => {
     const detections = await faceapi
-      .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions());
-    //  console.log(Object.keys(detections).length);
+      .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
 
- //   const resizedDetections = faceapi.resizeResults(detections, displaySize);
-  //  console.log(resizedDetections);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    console.log(resizedDetections);
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
-  //  faceapi.draw.drawDetections(canvas, resizedDetections);
-   // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      if (Object.keys(detections).length > 0) {
-
-          document.getElementById("Message").innerText = 'Hi Human Being';
-      }
-   else{
-       document.getElementById("Message").innerText = 'Not a Human Being';
-   }
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    if (resizedDetections && Object.keys(resizedDetections).length > 0) {
+      const age = resizedDetections.age;
+      const interpolatedAge = interpolateAgePredictions(age);
+      const gender = resizedDetections.gender;
+      const expressions = resizedDetections.expressions;
+      const maxValue = Math.max(...Object.values(expressions));
+      const emotion = Object.keys(expressions).filter(
+        item => expressions[item] === maxValue
+      );
       
-    
-  }, 5);
+          document.getElementById("Message").innerText = 'Hi Human Being';
+      
+   
+     //   document.getElementById("Message").innerText = 'Not a Human Being';
+   
+    }
+  }, 10);
 });
 
+function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30);
+  const avgPredictedAge =
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+  return avgPredictedAge;
+}
